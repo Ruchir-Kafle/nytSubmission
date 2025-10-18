5e4e94373a58d684f08d68af925da0c5fdafbe1b
Ruchir <ruchirkafle@gmail.com>
2025-10-18 18:25:40 -0400

    room and leonard

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ https://free3d.com/3d-model/-office-desk-v3--821728.html
 
 JOE!!!
 https://www.mixamo.com/#/?page=1&query=joe&type=Character
+
+room
+This work is based on "Manor Morning - Isometric Room" (https://sketchfab.com/3d-models/manor-morning-isometric-room-d9d06e1de00c4224b7c5b29e020302c4) by JackWolf248 (https://sketchfab.com/JackWolf248) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
--- aicutscene/main_scene.gd
+++ aicutscene/main_scene.gd
@@ -16,8 +16,26 @@ enum animations {
 
 var counter = 0
 var conversation = [
-	[characters.principal, "hey ruchir, how are you doing on this very fine day today?"],
-	[characters.you, "absolutely terrible. im apalled that you would even dare to ask.", animations.casual]
+	[characters.principal, "{you}, I've heard that you've been using AI in your classes."],
+	[characters.you, "..."],
+	[characters.principal, "You do understand that this is a serious offense? Plagiarism is no joke!", animations.casual],
+	[characters.you, "Yeah, yeah, yeah. I get it."],
+	[characters.principal, "I don't think you do, since this is your second offense. The first time we were gracious in letting you redo the assignment, but if you're going to continue this behavior, we can't keep being lenient.", animations.sit],
+	[characters.you, "..."],
+	[characters.principal, "{you}.", animations.leaning],
+	[characters.you, "..."],
+	[characters.principal, "Hey, come on, I'm trying to help you."],
+	[characters.you, "If you were trying to help me, you'd leave me alone."],
+	[characters.principal, "That's not helpful. Plagiarism is a crime that you can be expelled for. We're trying to teach you that."],
+	[characters.you, "Yeah right. Just shut up."],
+	[characters.principal, "..."],
+	[characters.principal, "Is that all you have to say for yourself?"],
+	[characters.you, "..."],
+	[characters.principal, "Well, if you're not going to cooperate with me, you leave me no choice but to expel you."],
+	[characters.you, "Huh?"],
+	[characters.principal, "It seems this is the only way to teach you a lesson."],
+	[characters.you, "Wait... you can't do that though."],
+	[characters.principal, "I just did. Now get out of my sight."]
 ]
 
 func _ready() -> void:
